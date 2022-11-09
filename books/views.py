f67624d73caec596b70604d22f092e1072d20ad0
danylott <d.tuytuyshkin@gmail.com>
2022-11-09 13:36:50 +0200

    Added permissions to Books

--- books/views.py
+++ books/views.py
@@ -1,9 +1,13 @@
 from rest_framework import viewsets
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from .models import Book
+from .permisssions import IsAdminUserOrReadOnly
 from .serializers import BookSerializer
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAdminUserOrReadOnly,)

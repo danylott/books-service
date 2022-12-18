0de38f081d0ec0d4aba1254530ec6d5d1a29b4d8
VladSnegovsky <50777219+VladSnegovsky@users.noreply.github.com>
2022-12-18 05:01:50 +0200

    Update tests.py

--- borrowings/tests.py
+++ borrowings/tests.py
@@ -44,3 +44,77 @@ class PublicBooksApiTests(TestCase):
 
         response = self.client.post(BOOK_URL, payload)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+class PrivateBookApiTests(TestCase):
+    def setUp(self):
+        self.user = create_user(
+            email="test@test.com",
+            password="testpass",
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_list_books(self):
+        sample_books()
+
+        response = self.client.get(BOOK_URL)
+
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
+
+    def test_post_books(self):
+        payload = {
+            "title": "Sample book",
+            "author": "Sample author",
+            "inventory": 10,
+            "daily_annual_fee": 1.00,
+        }
+
+        response = self.client.post(BOOK_URL, payload)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class AdminBookApiTests(TestCase):
+    def setUp(self):
+        self.user = create_user(
+            email="test@test.com",
+            password="testpass",
+            is_staff=True,
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_post_books(self):
+        payload = {
+            "title": "Sample book",
+            "author": "Sample author",
+            "inventory": 10,
+            "daily_annual_fee": 1.00,
+            "cover": "HARD",
+        }
+
+        response = self.client.post(BOOK_URL, payload)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_retrieve_book(self):
+        sample_books()
+
+        response = self.client.get(f"{BOOK_URL}1/")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_put_book(self):
+        sample_books()
+
+        response = self.client.put(f"{BOOK_URL}1/", {})
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_delete_book(self):
+        sample_books()
+
+        response = self.client.delete(f"{BOOK_URL}1/")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
